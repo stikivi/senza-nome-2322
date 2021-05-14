3310e65fc86b8b6d6a27a163ce0b9638f532656e
stikivi <84150137+stikivi@users.noreply.github.com>
2021-05-14 17:44:36 +0200

    Add files via upload

---
+++ Senza nome 23.bat
@@ -0,0 +1,18 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2580
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 HelveticaNeue;\f1\fnil\fcharset0 HelveticaNeue-Italic;}
+{\colortbl;\red255\green255\blue255;\red61\green68\blue82;\red52\green52\blue52;\red255\green255\blue255;
+\red6\green33\blue79;}
+{\*\expandedcolortbl;;\cssrgb\c30588\c33725\c39608;\csgray\c26515;\cssrgb\c100000\c100000\c100000;
+\cssrgb\c1176\c18431\c38431;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs34 \cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 lolMiner.exe --algo\'a0\ul ETHASH\'a0\ulnone --piscina\'a0\ul etchash\ulnone .unmineable.com: 3333 --utente\'a0
+\f1\i \ul BTT:
+\f0\i0 \cf3 \cb4 \expnd0\expndtw0\kerning0
+\ulnone \outl0\strokewidth0 TFPdz9yY5m7tNKZvmr9j6ccLTrtEX9gNEn.
+\f1\i \cf2 \cb1 \expnd0\expndtw0\kerning0
+\ul \ulc2 \outl0\strokewidth0 \strokec2 NBA
+\f0\i0 \ulnone \'a0-etstrato ETHPROXY}
